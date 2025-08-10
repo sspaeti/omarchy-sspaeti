3749db76e4f7d9773fd3c93ec239220b757eab0f
sspaeti <simon@ssp.sh>
2025-08-10 19:41:23 +0200

    install webapps

--- install/apps/webapps.sh
+++ install/apps/webapps.sh
@@ -12,4 +12,7 @@ if [ -z "$OMARCHY_BARE" ]; then
   ~/.local/share/omarchy/bin/omarchy-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
   ~/.local/share/omarchy/bin/omarchy-webapp-install "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
   ~/.local/share/omarchy/bin/omarchy-webapp-install "Figma" https://figma.com/ https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
+  ~/.local/share/omarchy/bin/omarchy-webapp-install "Bsky" https://blue.ssp.sh/ https://web-cdn.bsky.app/static/apple-touch-icon.png
+  ~/.local/share/omarchy/bin/omarchy-webapp-install "Claude" https://claude.ai https://claude.ai/images/claude_app_icon.png
+  ~/.local/share/omarchy/bin/omarchy-webapp-install "Grammarly" https://app.grammarly.com/ https://denali-static.grammarly.com/files/6dae2a07debfba035d6e/favicon-16x16-v2.png
 fi
